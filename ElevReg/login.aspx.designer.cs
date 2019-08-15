150953b1077042ae398caf2840fa4e51d95239c8
Emillarsen194 <47022984+Emillarsen194@users.noreply.github.com>
2019-08-15 12:14:29 +0200

    sss

--- ElevReg/login.aspx.designer.cs
+++ ElevReg/login.aspx.designer.cs
@@ -13,6 +13,24 @@ namespace ElevReg {
     public partial class Login {
         
         /// <summary>
+        /// uname control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox uname;
+        
+        /// <summary>
+        /// passw control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox passw;
+        
+        /// <summary>
         /// Button1 control.
         /// </summary>
         /// <remarks>
@@ -20,5 +38,14 @@ namespace ElevReg {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
